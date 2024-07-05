8deb78c02614a62cddd49915f71b76d73d8adf59
Max London <59746406+weeturtle@users.noreply.github.com>
2024-07-05 14:01:30 +0100

    Delete dist directory

--- dist/helper.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const yo = "yo";
-exports.default = yo;
-//# sourceMappingURL=helper.js.map
--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const helper_1 = __importDefault(require("./helper"));
-console.log(helper_1.default);
-console.log("Hello World!");
-//# sourceMappingURL=index.js.map
